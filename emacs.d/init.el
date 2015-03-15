ef6fa3fba201f35d354f6e83ce1b2ddd16e3c333
Jamie Curran <jay.curran@gmail.com>
2015-03-15 15:04:11 +0000

    Added clojure related packages.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,21 @@
 (require 'package)
+(package-initialize)
 (add-to-list 'package-archives
 	     '("melpha-stable" . "http://melpa-stable.milkbox.net/packages/"))
-(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(clojure-mode
+		      projectile
+		      smartparens
+		      cider
+		      exec-path-from-shell))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
