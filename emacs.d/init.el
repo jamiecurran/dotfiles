8a31acbe66ec54f2da1e41f9e2ccd03528d32cd3
Jamie Curran <jay.curran@gmail.com>
2015-12-22 17:04:44 +0000

    Updated emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -24,7 +24,10 @@
 		     haskell-mode
 		     rainbow-delimiters
 		     impatient-mode
-		     dockerfile-mode))
+		     dockerfile-mode
+		     web-mode
+		     helm
+		     helm-projectile))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
@@ -41,7 +44,50 @@
 (add-hook 'cider-repl-mode-hook ' rainbow-delimiters-mode)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 2))
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(custom-set-variables
+ '(terraform-indent-level 4))
+
+(fset 'insertPound "#")
+(global-set-key (kbd "M-3") 'insertPound)
+
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
+(require 'helm)
+(require 'helm-config)
+(require 'helm-projectile)
+(helm-mode 1)
+
 (projectile-global-mode)
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+(setq projectile-switch-project-action 'helm-projectile)
 
 (setq cider-repl-use-clojure-font-lock t)
 
@@ -52,6 +98,9 @@
 (global-linum-mode t)
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(add-hook 'shell-mode-hook
+      	  'ansi-color-for-comint-mode-on)
 
 (require 'moe-theme)
 (moe-dark)
